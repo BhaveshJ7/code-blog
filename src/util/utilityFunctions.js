8ddb4dde5b7e26ff32821a3d26186884934e75f0
BhaveshJ7 <bhavesh070302@gmail.com>
2024-03-04 23:32:31 +0530

    modified slugify function

--- src/util/utilityFunctions.js
+++ src/util/utilityFunctions.js
@@ -4,9 +4,13 @@ const slugify = function (text) {
     .toLowerCase()
     .replace(/\s+/g, "-") // Replace spaces with -
     .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/[&?]+/g, "") // Remove colons, ampersands, and question marks
     .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, "") // Trim - from end of text
+    .replace(/:/g, "_") // Replace colons with underscores
 }
 
 module.exports = { slugify }
