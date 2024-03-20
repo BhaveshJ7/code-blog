1d09701c38a3865a588b4bb178ae92b6de21f553
BhaveshJ7 <bhavesh070302@gmail.com>
2024-03-20 10:54:54 +0530

    start_url removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,6 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: ``,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
