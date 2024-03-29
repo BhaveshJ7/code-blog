66f559b7d7bd24bc451bab27b863b68cb4b54588
BhaveshJ7 <bhavesh070302@gmail.com>
2024-03-29 10:21:22 +0530

    config file changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,14 @@ module.exports = {
       },
     },
     {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: `uploads`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
